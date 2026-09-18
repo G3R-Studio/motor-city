87d3dbc4b5a89f47c0eb73e3a3d9a73f44a4bb3d
Exsall <ziya19999@gmail.com>
2026-09-18 02:31:16 +0200

    Widen sprint route with city footprint

--- Assets/Scripts/Gameplay/StreetSprintActivity.cs
+++ Assets/Scripts/Gameplay/StreetSprintActivity.cs
@@ -9,8 +9,8 @@ namespace MotorCity.Gameplay
 
         [SerializeField] private int baseRewardCredits = 550;
         [SerializeField] private int maximumTimeBonusCredits = 450;
-        [SerializeField] private float startRadius = 10.5f;
-        [SerializeField] private float checkpointRadius = 9f;
+        [SerializeField] private float startRadius = 18.9f;
+        [SerializeField] private float checkpointRadius = 16.2f;
 
         private ArcadeCarController car;
         private PlayerWallet wallet;
@@ -36,12 +36,12 @@ namespace MotorCity.Gameplay
             activityManager = manager;
             route = new[]
             {
-                new Vector3(-126f, 0f, -126f),
-                new Vector3(-126f, 0f, 63f),
-                new Vector3(0f, 0f, 126f),
-                new Vector3(126f, 0f, 63f),
-                new Vector3(126f, 0f, -63f),
-                new Vector3(0f, 0f, -126f)
+                new Vector3(-226.6f, 0f, -226.6f),
+                new Vector3(-226.6f, 0f, 113.3f),
+                new Vector3(0f, 0f, 226.6f),
+                new Vector3(226.6f, 0f, 113.3f),
+                new Vector3(226.6f, 0f, -113.3f),
+                new Vector3(0f, 0f, -226.6f)
             };
         }
 
