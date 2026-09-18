6fea84ad9afd938dbe148e3af821e655c0a9e484
Exsall <ziya19999@gmail.com>
2026-09-18 02:40:08 +0200

    Scale sprint route another 10 percent

--- Assets/Scripts/Gameplay/StreetSprintActivity.cs
+++ Assets/Scripts/Gameplay/StreetSprintActivity.cs
@@ -9,8 +9,8 @@ namespace MotorCity.Gameplay
 
         [SerializeField] private int baseRewardCredits = 550;
         [SerializeField] private int maximumTimeBonusCredits = 450;
-        [SerializeField] private float startRadius = 30.5f;
-        [SerializeField] private float checkpointRadius = 26.1f;
+        [SerializeField] private float startRadius = 33.6f;
+        [SerializeField] private float checkpointRadius = 28.7f;
 
         private ArcadeCarController car;
         private PlayerWallet wallet;
@@ -36,12 +36,12 @@ namespace MotorCity.Gameplay
             activityManager = manager;
             route = new[]
             {
-                new Vector3(-365.4f, 0f, -365.4f),
-                new Vector3(-365.4f, 0f, 182.6f),
-                new Vector3(0f, 0f, 365.4f),
-                new Vector3(365.4f, 0f, 182.6f),
-                new Vector3(365.4f, 0f, -182.6f),
-                new Vector3(0f, 0f, -365.4f)
+                new Vector3(-401.9f, 0f, -401.9f),
+                new Vector3(-401.9f, 0f, 200.9f),
+                new Vector3(0f, 0f, 401.9f),
+                new Vector3(401.9f, 0f, 200.9f),
+                new Vector3(401.9f, 0f, -200.9f),
+                new Vector3(0f, 0f, -401.9f)
             };
         }
 
