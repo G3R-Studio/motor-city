acbd6fca545773665e77ae0eb2b9677816d6bfe4
Exsall <ziya19999@gmail.com>
2026-09-18 02:34:44 +0200

    Expand ground collider for larger uniform city

--- Assets/Scripts/World/CityAssetRuntimeInstaller.cs
+++ Assets/Scripts/World/CityAssetRuntimeInstaller.cs
@@ -82,7 +82,7 @@ namespace MotorCity.World
                 ground.AddComponent<BoxCollider>();
 
             collider.size =
-                new Vector3(680f, 0.4f, 680f);
+                new Vector3(960f, 0.4f, 960f);
         }
     }
 }
