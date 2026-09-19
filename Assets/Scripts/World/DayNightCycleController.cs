2ffa61f96792ab5e2d0cf6a1656314b020be5a3d
Exsall <ziya19999@gmail.com>
2026-09-19 22:47:22 +0200

    Log runtime street lamp activation counts

--- Assets/Scripts/World/DayNightCycleController.cs
+++ Assets/Scripts/World/DayNightCycleController.cs
@@ -110,6 +110,11 @@ namespace MotorCity.World
 
                 ResolveLampObserver();
                 ApplyStreetLights();
+
+                Debug.Log(
+                    "Motor City: street lamps " +
+                    $"night={IsNight}, amount={NightAmount:0.00}, " +
+                    $"found={StreetLightCount}, enabled={EnabledStreetLightCount}.");
             }
         }
 
