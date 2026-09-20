a7e14c28ee3a6f92db42f8f8dd52bcd1dd26cd83
Exsall <ziya19999@gmail.com>
2026-09-20 18:16:07 +0200

    Enable FCG traffic performance optimization

--- Assets/Scripts/Bootstrap/MotorCityBootstrap.cs
+++ Assets/Scripts/Bootstrap/MotorCityBootstrap.cs
@@ -52,6 +52,14 @@ namespace MotorCity.Bootstrap
             BindFcgTrafficPlayer(
                 car.transform);
 
+            FcgTrafficPerformanceOptimizer trafficOptimizer =
+                new GameObject(
+                    "FCG Traffic Performance Optimizer")
+                    .AddComponent<FcgTrafficPerformanceOptimizer>();
+
+            trafficOptimizer.Initialize(
+                car);
+
             VehiclePositionPersistence positionPersistence =
                 car.gameObject.AddComponent<VehiclePositionPersistence>();
 
