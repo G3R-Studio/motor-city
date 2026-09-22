62ae7c5bc5414aa37eb8bfa1cae22f56ceb60b6c
Exsall <ziya19999@gmail.com>
2026-09-22 14:13:23 +0200

    Increase speedometer background transparency

--- Assets/Scripts/UI/PrototypeHud.cs
+++ Assets/Scripts/UI/PrototypeHud.cs
@@ -1869,7 +1869,7 @@ namespace MotorCity.UI
                         0.01f,
                         0.016f,
                         0.026f,
-                        0.74f));
+                        0.42f));
 
             CreateAccent(
                 panel,
