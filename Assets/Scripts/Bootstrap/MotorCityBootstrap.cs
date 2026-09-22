7435c2d867d5ec0d78b82c9b06506077e85cee69
Exsall <ziya19999@gmail.com>
2026-09-23 00:44:52 +0200

    Add audio listener to runtime camera

--- Assets/Scripts/Bootstrap/MotorCityBootstrap.cs
+++ Assets/Scripts/Bootstrap/MotorCityBootstrap.cs
@@ -1553,6 +1553,7 @@ namespace MotorCity.Bootstrap
             GameObject cameraObject = new("Main Camera");
             cameraObject.tag = "MainCamera";
             Camera camera = cameraObject.AddComponent<Camera>();
+            cameraObject.AddComponent<AudioListener>();
             camera.fieldOfView = 62f;
             camera.nearClipPlane = 0.12f;
             camera.farClipPlane = 2200f;
